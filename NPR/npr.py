7604b3e5d8e724e05a4d9a293e69d67df2eaaf4f
Brian Yang <brianyang1106@gmail.com>
2014-01-20 01:39:46 -0600

    add xml parser

--- NPR/npr.py
+++ NPR/npr.py
@@ -2,6 +2,7 @@
 #coding:utf-8
 
 from urllib2 import Request, urlopen, URLError
+from xml.dom import minidom
 
 def npr():
 
@@ -13,9 +14,19 @@ def npr():
     except URLError, e:
         print 'No kittez. Got an error code:', e
 
+def xml_parser (filename):
+    f = open(filename, 'r')
+    pets = minidom.parseString(f.read())
+    f.close()
+
+    names = pets.getElementsByTagName('name')
+    for name in names:
+	print name.firstChild.nodeValue
+
 # main
 def main():
     npr()
+    xml_parser("pets.txt")
 
 # call main()
 if __name__ == '__main__':
